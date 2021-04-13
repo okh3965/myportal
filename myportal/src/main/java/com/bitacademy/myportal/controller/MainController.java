9ec2a8f158db958642c7de15c2e40628f04fc05b
okyunghwan <okyeonghwan5@gmail.com>
2021-04-13 15:04:49 +0900

    myportal first commit

---
+++ myportal/src/main/java/com/bitacademy/myportal/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.bitacademy.myportal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/main")
+	public ModelAndView home(@RequestParam String name) {
+		
+		ModelAndView mav = new ModelAndView();
+		
+//		mav.addObject("message", "This portal is " + name + "'s portal");
+		mav.setViewName("/WEB-INF/views/home.jsp");
+		
+		return mav;	
+	}
+}
